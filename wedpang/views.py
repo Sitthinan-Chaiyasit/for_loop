6585ac93fb45304dede4bf73939ffe178abd338a
phattharawadee24 <phattharawadeess283@gmail.com>
2025-08-04 11:51:16 +0700

    first commit

---
+++ wedpang/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def for_view(request):
+    context = {}
+    context['message'] = "การวนซ้ำใน_Django"
+    
+    if request.method == 'POST' and request.POST.get('count') != '':
+        number = int(request.POST.get('count'))
+        context['count'] = list(range(1,number +1))
+    else:
+        context['count'] = list(range(1, 2))
+        
+    return render(request, 'for.html',context)
+
+def multipli(request):
+    context = {}
+    context['message'] = "ตารางสูตรคูณ"
+    
+    number = 1
+    if request.method == 'POST' and request.POST.get('number') != '':
+        try:
+            number = int(request.POST.get('number'))
+        except ValueError:
+            number = 1
+    context['number'] = number
+    context['table'] = [(i, number * i) for i in range(1, 13)]
+        
+    return render(request, 'multiplication.html', context)
