062070d724e9650b2f8c935190b94fec3f4b9567
phattharawadee24 <phattharawadeess283@gmail.com>
2025-09-04 15:32:14 +0700

    pp24

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,10 @@
+#!/bin/bash
+
+# ติดตั้ง dependencies
 python3.12 -m pip install -r requirements.txt
+
+# เก็บ static files
 python3.12 manage.py collectstatic --noinput --clear
+
+# รัน migrations (แนะนำให้เพิ่ม)
+python3.12 manage.py migrate
