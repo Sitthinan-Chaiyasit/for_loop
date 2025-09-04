8082c970a5d25e11330ec614dfb58c5e50a54587
phattharawadee24 <phattharawadeess283@gmail.com>
2025-09-04 16:41:44 +0700

    fix vercel

--- wedpang/settings_dev.py
+++ wedpang/settings_dev.py
@@ -92,7 +92,6 @@ WSGI_APPLICATION = 'wedpang.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-<<<<<<< HEAD
      'default': {
          'ENGINE': 'django.db.backends.sqlite3',
          'NAME': BASE_DIR / 'db.sqlite3',
@@ -100,7 +99,6 @@ DATABASES = {
 }
 
 
-=======
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
@@ -111,7 +109,6 @@ DATABASES = {
     }
 }
 
->>>>>>> feature-yyy
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
