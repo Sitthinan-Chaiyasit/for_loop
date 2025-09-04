ca47f1bb635abde423059f552495e7a1fcd63a0d
phattharawadee24 <phattharawadeess283@gmail.com>
2025-09-04 15:45:21 +0700

    pp242

--- build_files.sh
+++ build_files.sh
@@ -1,20 +1,2 @@
-#!/bin/bash
-set -e  # Stop on error
-
-# Build the project
-echo "Building the project..."
-pip install -r requirements.txt
-
-# Make sure the static directory exists
-echo "Creating static directories..."
-mkdir -p static
-mkdir -p staticfiles_build
-
-# Collect static files
-echo "Collecting static files..."
-python manage.py collectstatic --no-input
-
-# Run database migrations
-echo "Running database migrations..."
-python manage.py makemigrations
-python manage.py migrate
+python3.12 -m pip install -r requirements.txt
+python3.12 manage.py collectstatic --noinput --clear
